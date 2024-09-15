e61335e128ae8626e04209e310581aa58a927867
Habiba <habiba5543@live.com>
2024-09-15 07:52:56 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/thirtydays/data/DataRepository.kt
@@ -0,0 +1,37 @@
+package com.example.thirtydays.data
+
+import com.example.thirtydays.R
+import com.example.thirtydays.model.Tip
+
+val Tips = listOf(
+    Tip(1, R.string.tip_1_title, R.string.tip_1_description, R.drawable.wakeup),
+    Tip(2, R.string.tip_2_title, R.string.tip_2_description, R.drawable.water),
+    Tip(3, R.string.tip_3_title, R.string.tip_3_description, R.drawable.placeholder),
+    Tip(4, R.string.tip_4_title, R.string.tip_4_description, R.drawable.placeholder),
+    Tip(5, R.string.tip_5_title, R.string.tip_5_description, R.drawable.placeholder),
+    Tip(6, R.string.tip_6_title, R.string.tip_6_description, R.drawable.placeholder),
+    Tip(7, R.string.tip_7_title, R.string.tip_7_description, R.drawable.placeholder),
+    Tip(8, R.string.tip_8_title, R.string.tip_8_description, R.drawable.placeholder),
+    Tip(9, R.string.tip_9_title, R.string.tip_9_description, R.drawable.placeholder),
+    Tip(10, R.string.tip_10_title, R.string.tip_10_description, R.drawable.placeholder),
+    Tip(11, R.string.tip_11_title, R.string.tip_11_description, R.drawable.placeholder),
+    Tip(12, R.string.tip_12_title, R.string.tip_12_description, R.drawable.placeholder),
+    Tip(13, R.string.tip_13_title, R.string.tip_13_description, R.drawable.placeholder),
+    Tip(14, R.string.tip_14_title, R.string.tip_14_description, R.drawable.placeholder),
+    Tip(15, R.string.tip_15_title, R.string.tip_15_description, R.drawable.placeholder),
+    Tip(16, R.string.tip_16_title, R.string.tip_16_description, R.drawable.placeholder),
+    Tip(17, R.string.tip_17_title, R.string.tip_17_description, R.drawable.placeholder),
+    Tip(18, R.string.tip_18_title, R.string.tip_18_description, R.drawable.placeholder),
+    Tip(19, R.string.tip_19_title, R.string.tip_19_description, R.drawable.placeholder),
+    Tip(20, R.string.tip_20_title, R.string.tip_20_description, R.drawable.placeholder),
+    Tip(21, R.string.tip_21_title, R.string.tip_21_description, R.drawable.placeholder),
+    Tip(22, R.string.tip_22_title, R.string.tip_22_description, R.drawable.placeholder),
+    Tip(23, R.string.tip_23_title, R.string.tip_23_description, R.drawable.placeholder),
+    Tip(24, R.string.tip_24_title, R.string.tip_24_description, R.drawable.placeholder),
+    Tip(25, R.string.tip_25_title, R.string.tip_25_description, R.drawable.placeholder),
+    Tip(26, R.string.tip_26_title, R.string.tip_26_description, R.drawable.placeholder),
+    Tip(27, R.string.tip_27_title, R.string.tip_27_description, R.drawable.placeholder),
+    Tip(28, R.string.tip_28_title, R.string.tip_28_description, R.drawable.placeholder),
+    Tip(29, R.string.tip_29_title, R.string.tip_29_description, R.drawable.placeholder),
+    Tip(30, R.string.tip_30_title, R.string.tip_30_description, R.drawable.placeholder)
+)
